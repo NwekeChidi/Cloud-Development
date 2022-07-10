4accd72ea8f4d4d389cc96fee4994148c96fbe7a
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-07-10 16:37:45 +0100

    applied reviewed changes

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -30,23 +30,20 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  app.get( "/filteredimage", async (req, res ) => {
-    const { image_url } = req.query;
+  app.get( "/filteredimage", async (req: Request, res: Response ) => {
+    const { image_url }: { image_url:string } = req.query;
     if (!image_url) return res.status(400).send({
       status: "Failed",
       error: "Url of image to be filtered not found!"
     });
 
     const filteredImagePath = await filterImageFromURL(image_url);
-    // console.log("THis is the filtered path:", filteredImagePath)
-    res.sendFile(filteredImagePath);
-    //await deleteLocalFiles([filteredImagePath]);
-    return;
+    res.status(200).sendFile(filteredImagePath);
   })
   
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req: Request, res: Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
