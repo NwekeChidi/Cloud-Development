96486ccfa7c6d0bbb1706f1f99e0a2770190f0b2
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-07-10 12:15:15 +0100

    completed project

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -30,6 +30,18 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
+  app.get( "/filteredimage", async (req, res ) => {
+    const { image_url } = req.query;
+    if (!image_url) return res.status(400).send({
+      status: "Failed",
+      error: "Url of image to be filtered not found!"
+    });
+
+    const filteredImagePath = await filterImageFromURL(image_url);
+    res.sendFile(filteredImagePath);
+    //await deleteLocalFiles([filteredImagePath]);
+    return;
+  })
   
   // Root Endpoint
   // Displays a simple message to the user
